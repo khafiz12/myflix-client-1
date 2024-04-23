35dc61249900f625335fbea9d114a3b9f7256087
khafiz12 <karimhafiz7@gmail.com>
2024-04-23 17:26:17 -0400

    changes to exercise 3.4

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,6 @@
 //check movieData to see if its movie or moviedata//
+import{useState,useEffect} from "react";
+import PropTypes from "prop-types";
 export const MovieCard = ({movie, onMovieClick}) => {
     return ( 
     <div onClick ={() => {
@@ -6,6 +8,20 @@ export const MovieCard = ({movie, onMovieClick}) => {
     }}
     >
         {movie.title}
+
     </div>
     );
+};
+
+MovieCard.PropTypes = { 
+    movie: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired,
+    featured: PropTypes.string.bool
+    }).isRequired, 
+    onMovieClick:PropTypes.func.isRequired
 };
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,3 +1,5 @@
+import {useState, useEffect} from "react";
+import PropTypes from "prop-types";
 export const MovieView = ({movie, onBackClick}) => {
     return (
         <div>
@@ -9,10 +11,53 @@ export const MovieView = ({movie, onBackClick}) => {
                 <span>{movie.title}</span>
             </div>
             <div>
-                <span>director: </span>
+           <span>Name:</span>
+           <span>{movie.director.name}</span>
+            </div>
+        <div>
+          <span>Bio:</span>
+          <span>{movie.director.bio}</span>
+        </div>
+          <div>
+            <span>Birthday:</span>
+            <span>{movie.director.birthday}</span>
+          </div>
+          <div>
+           <span>Deathday:</span>
+           <span>{movie.director.deathday}</span>
+         </div>
+            <div>
+                <span>genre</span>
+                <span>{movie.genre}</span>
+            </div>
+            <div>
+                <span>description</span>
+                <span>{movie.description}</span>
+            </div>
+            <div>
+                <span>director</span>
                 <span>{movie.director}</span>
             </div>
+
             <button onClick={onBackClick}>Back button</button>
         </div>
     );
+};
+
+MovieView.propTypes = { 
+    movie: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    director: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        bio: PropTypes.string.isRequired,
+        birthday: PropTypes.string.isRequired,
+        deathday: PropTypes.string,
+    }).isRequired,
+    featured: PropTypes.string.bool
+    }).isRequired, 
+    onBackClick: PropTypes.func.isRequired
 };
