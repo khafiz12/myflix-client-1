83f4368ceb959d2f3e8caca6f0d3d84a95968fd5
khafiz12 <karimhafiz7@gmail.com>
2024-05-01 22:26:10 -0400

    corrected function syntax.
    corrected functions into function.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require ('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = functions(app); {
+module.exports = function(app) {
     app.use(
         "/login", 
-     createProxyMiddleware({
-       target:"https://top-movies-flix-0061641eb1b3.herokuapp.com",
+        createProxyMiddleware({
+       target: "https://top-movies-flix-0061641eb1b3.herokuapp.com",
        changeOrigin: true, 
      })
    );
